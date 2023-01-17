7423bc3b83baf873b218ab8ab81b4d54b9511bbd
Guy Schvitz <guyschvitz@Guys-MacBook-Air.fritz.box>
2023-01-17 19:09:34 +0100

    add function to check which UCDP datasets currently available

---
+++ R/checkUcdpAvailable.R
@@ -0,0 +1,67 @@
+#' checkUcdpAvailable: Check which UCDP datasets are currently available
+#'
+#' @param dataset character. Name of UCDP dataset. Needs to be one of the following:
+#' "ucdpprioconflict", "battledeaths", "dyadic", "nonstate", "onesided" (yearly data),
+#' or "gedevents" (daily data, released in monthly batches)
+#' The API does not require an account/key but there are some usage limitations
+#' For more information, see: https://ucdp.uu.se/apidocs/
+#'
+#' @param version character. Dataset version number.
+#' For yearly UCDP data: Format: YY.1 (e.g. 22.1 for data released in 2022)
+#' For monthly UCDP GED data use format: YY.0.MM (e.g. 22.0.11 for data on November 2022)
+#' For quarterly UCDP releases data use format: YY.01.YY.MM (e.g. 22.01.22.09 for data from Jan to Sep 2022)
+#'
+#' @return data.frame with requested UCDP dataset
+#' @export
+#'
+#' @examples
+#' ## Check which monthly UCDP GED candidate datasets are available
+#' ## m.ids <- 1:12
+#' gedc.m.check.df <- do.call(rbind, lapply(m.ids, function(x){
+#'   checkUcdpAvailable("gedevents", sprintf("22.0.%s", x))
+#'   }))
+#'
+#' ## Check which quarterly UCDP GED candidate datasets are available
+#' q.ids <- sprintf("%02d", seq(3, 12, 3))
+#' gedc.q.check.df <- do.call(rbind, lapply(q.ids, function(x){
+#'   checkUcdpAvailable("gedevents", sprintf("22.01.22.%s", x))
+#'   }))
+#'
+checkUcdpAvailable <- function(dataset, version){
+
+  ## Stop if requested pagesize greater than 1000
+  if(pagesize > 1000){
+    stop("Page size cannot exceed 1000")
+  }
+
+  ## Check if dataset name is valid
+  dataset.names <- c("ucdpprioconflict", "battledeaths", "dyadic", "nonstate",
+                     "onesided", "gedevents")
+
+  dataset.string <- paste(paste0("'", dataset.names, "'"), collapse = ", ")
+
+  if(!dataset %in% dataset.names){
+    stop(sprintf("dataset name needs to be one of the following:\n%s",
+                 dataset.string))
+  }
+
+  ## Build initial URL: Insert dataset number, version and pagesize
+  url <- sprintf("https://ucdpapi.pcr.uu.se/api/%s/%s?pagesize=100",
+                 dataset, version)
+
+  ## ping URL
+  url.ping <- tryCatch(
+    httr::GET(url),
+    error = function(e){e})
+
+  ## Check if query returns an error or not
+  if(any(class(url.ping) == "error") | url.ping$status_code %in% 400:504) {
+    exists <- F
+  } else {
+    exists <- T
+  }
+
+  ## Collect name of data, version and result of ping
+  out.df <- data.frame(dataset, version, exists)
+  return(out.df)
+}
